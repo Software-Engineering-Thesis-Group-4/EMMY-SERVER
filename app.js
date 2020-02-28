26c50b46dd4e1d53ec5e56739c26575f9b8ce6f6
Paolo Latoja <26647797+paololatoja@users.noreply.github.com>
2020-02-28 17:40:53 +0800

    initial changes to server -pao

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const createError  = require('http-errors');
 const http         = require('http');
-const cookieParser = require('cookie-parser');
 const path         = require('path');
 const logger       = require('morgan');
 const socketIO     = require('socket.io');
 const cors         = require('cors');
 const express      = require('express');
+const session 		 = require('express-session');
+const MongoStore 	 = require('connect-mongo')(session);
+
+import { v4 as uuidv4 } from 'uuid';
 
 // enable .env config variables
 require('dotenv').config();
@@ -23,8 +26,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser());
 app.use(cors());
+app.use(morgan('dev'));
 
 // DATABASE ---------------------------------------------------------------------------------------
 const { createDBConnection } = require('./db');
@@ -40,6 +43,32 @@ app.use('/', indexRoute); // localhost:3000/
 app.use('/auth', authRoute); // localhost:3000/auth/
 app.use('/api/employees', employeeRoute); // localhost:3000/api/employees/
 app.use('/api/employeelogs', employeeLogsRoute);// localhost:3000/api/employeelogs/
+
+const user_id = `user-${uuidv4()}`;
+
+// SESSION AND MIDDLEWARE
+app.use(session({
+	store: new MongoStore({ mongooseConnection: mongoose.connection}),
+	key: user_id,
+	name: process.env.SESSION_NAME,
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		maxAge: parseInt(process.env.SESSION_DURATION), // 1hr from .env
+		sameSite: false // cors
+	}
+}));
+
+
+
+app.use((req, res, next) => {
+	if (req.cookies.cookie === 0 && !req.session.user) {
+		 res.clearCookie('user_id');
+	}
+	next();
+});
+
 
 
 
@@ -64,7 +93,7 @@ app.use((err, req, res, next) => {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
-	
+
 	// render the error page
 	res.status(err.status || 500);
 	res.render('error');
--- routes/auth.js
+++ routes/auth.js
@@ -8,20 +8,21 @@ const { User } = require('../db/models/User');
 
 
 module.exports = (io) => {
-	
+
 	// POST /auth
 	// Description: authenticate admin user (for login)
 	router.post('/', (req, res) => {
-		username = req.body.username;
-		password = req.body.password;
-		
-		User.findOne({ username }, (err, user) => {
+		let email = req.body.email;
+
+		User.findOne({ email }, (err, user) => {
 			if(err) return res.status(500).send({ message: 'Error on the server.' });
-			if(!user) return res.status(404).send({ message: `User does not exist.`});
-			
+			if(!user) return res.status(404).send({ message: `User ${email} does not exist.`});
+
+			let password = req.body.password;
+
 			// compare password
 			let validPassword = bcrypt.compareSync(password, user.password);
-			
+
 			if(!validPassword) {
 				//
 				res.status = 401;
@@ -31,38 +32,50 @@ module.exports = (io) => {
 					message : "Invalid username or password."
 				});
 			}
-			
-			// sign token
-			let token = jwt.sign({ id: user._id }, process.env.JWT_KEY, {
-				expiresIn: 86400 // expires in 24 hours
-			});
-			
-			let { username, firstname, lastname } = user;
-			
-			res.status(200).send({ token, username, firstname, lastname });
+			else {
+				// sign token
+				let token = jwt.sign({ id: user._id }, process.env.JWT_KEY, {
+					expiresIn: 86400 // expires in 24 hours
+				});
+
+				let { email, firstname, lastname } = user;
+
+				req.session.user.id = user._id;
+				req.session.user.name = firstname + lastname
+
+				res.status(200).send({ token, email, firstname, lastname });
+			}
+
+
 		});
 	});
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+	router.post('/resetpassword', (req, res) => {
+		let email = req.body.email;
+
+		User.findOne({ email }, (err, user) => {
+
+		});
+	});
+
+
+
+
+
+
+
+
 	// debugging --------------------------------------------------------------------------------------------------------------------------
-	router.post('/enroll', async (req, res) => {      
+	router.post('/enroll', async (req, res) => {
 		try {
 			// get username and hash password using bcrypt
-			let { 
-				username, 
-				firstname, 
+			let {
+				username,
+				firstname,
 				lastname } = req.body;
 			let $_hashedPassword = bcrypt.hashSync(req.body.password, 8);
-			
+
 			// create a new user of type [User]
 			let newUser = new User({
 				username: username,
@@ -70,22 +83,22 @@ module.exports = (io) => {
 				lastname: lastname,
 				password: $_hashedPassword
 			});
-			
+
 			// save user to db
 			let user = await newUser.save();
-			
+
 			var token = jwt.sign({ id: user._id }, process.env.JWT_KEY, {
 				expiresIn: 86400 //expires in 24 hours
 			});
-			
+
 			res.status(200).send({ auth: true, token });
-			
+
 		} catch (error) {
 			console.log(error);
 			res.status(500).send("There was a problem registering the user.");
 		}
 	});
-	
-	
+
+
 	return router;
 }
