75594080af9da6098aca7b3c767ad906e628ac8a
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-03-06 16:09:51 +0800

    removed cookieParser middleware

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-const createError = require('http-errors');
-const http = require('http');
-const path = require('path');
-const logger = require('morgan');
-const socketIO = require('socket.io');
-const cors = require('cors');
-const express = require('express');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
+const createError  = require('http-errors');
+const http         = require('http');
+const path         = require('path');
+const logger       = require('morgan');
+const socketIO     = require('socket.io');
+const cors         = require('cors');
+const express      = require('express');
+const session      = require('express-session');
+const MongoStore   = require('connect-mongo')(session);
+const mongoose     = require('mongoose');
+const dotenv       = require('dotenv');
 
 dotenv.config();
 
@@ -29,7 +28,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-app.use(cookieParser());
 
 // DATABASE ---------------------------------------------------------------------------------------
 const { createDBConnection } = require('./db');
@@ -45,7 +43,8 @@ const authRoute = require('./routes/auth')(io);
 app.use(session({
 	store: new MongoStore({
 		mongooseConnection: mongoose.connection,
-		collection: 'sessions'
+		collection: 'sessions',
+		autoRemove: 'native'
 	}),
 	name: process.env.SESSION_NAME,
 	secret: process.env.SESSION_SECRET,
