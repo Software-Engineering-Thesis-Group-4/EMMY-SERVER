a208ea1e2242519480cc0d34ab92be65ae844592
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-03-07 09:05:49 +0800

    removed unused variable "newLog"

--- utility/create-employee-log.js
+++ utility/create-employee-log.js
@@ -22,7 +22,7 @@ module.exports = (io, fingerprintNumber) => {
                 });
             }
 
-            // [2] if employee doesn't have any latest log
+            // [2] if latestLog "reference" field is not initialized (default: employee.latestLog == null)
             if (!employee.latestLog) {
 
                 let employeeLog = new EmployeeLog({
@@ -30,9 +30,9 @@ module.exports = (io, fingerprintNumber) => {
                     employeeId: employee.employeeId
                 });
 
-                let newLog = await employeeLog.save();
+                await employeeLog.save();
 
-                // initialize employee's latest log
+                // initialize employee's latest log "reference"
                 employee.latestLog = {
                     reference: employeeLog._id,
                     date: dateNow
@@ -51,11 +51,13 @@ module.exports = (io, fingerprintNumber) => {
                     message: `${employee.firstName} ${employee.lastName} checked in at ${dateNow.toLocaleDateString()}.`
                 });
 
-            } else { // get the employee's latestLog
+            } else {
 
+                // if the latestLog "reference" is initialized, get employeeLog document from db.
                 let employeeLatestLog = await EmployeeLog.findById(employee.latestLog.reference);
 
-                // if employee's latestLog is not found (deleted), create a new one
+                // if the "reference" it initialized but employeeLog is not found or deleted...
+                // CREATE a new employeeLog and set the latestLog "reference" of the employee
                 if (!employeeLatestLog) {
 
                     let employeeLog = new EmployeeLog({
@@ -63,7 +65,7 @@ module.exports = (io, fingerprintNumber) => {
                         employeeId: employee.employeeId,
                     });
 
-                    let newLog = await employeeLog.save();
+                    await employeeLog.save();
 
                     employee.latestLog = {
                         reference: employeeLog._id,
@@ -83,8 +85,11 @@ module.exports = (io, fingerprintNumber) => {
                         message: `Log not found (deleted) \n${employee.firstName} ${employee.lastName} checked in at ${dateNow.toLocaleDateString()}.`
                     });
 
-                } else { // if the lastest log exists, check if the employee is currently logged in
+                } else {
 
+                    // else, if the employeeLog document exists...
+                    // check if the employeeLog time-in is already past 24 hours (1 day)
+                    // this means that the time-out is overdue
                     let timeIn = employeeLatestLog.in;
                     let timeOut = employeeLatestLog.out;
 
@@ -92,66 +97,63 @@ module.exports = (io, fingerprintNumber) => {
                     let dateTomorrow = new Date();
                     dateTomorrow.setDate(dateNow.getDate() + 1);
 
-                    if (timeIn) {
-                        
-                        if (timeIn > dateTomorrow) {
+                    if (timeIn > dateTomorrow) {
 
-                            let employeeLog = new EmployeeLog({
-                                employee: employee._id,
-                                employeeId: employee.employeeId,
-                            });
+                        let employeeLog = new EmployeeLog({
+                            employee: employee._id,
+                            employeeId: employee.employeeId,
+                        });
 
-                            let newLog = await employeeLog.save();
+                        await employeeLog.save();
 
-                            employee.latestLog = {
-                                reference: employeeLog._id,
-                                date: dateNow
-                            }
-
-                            await employee.save();
-
-                            io.sockets.emit('employeeLog', {
-                                reference: employeeLog._id,
-                                employee: employee.firstName,
-                                status: 'in'
-                            });
-
-                            return resolve({
-                                status: 200,
-                                message: `Employee did not check-out yesterday at ${dateNow.toLocaleDateString()}. \n${employee.firstName} ${employee.lastName} checked in.`
-                            });
-                            
-                        } else if (!timeOut) {
-                            employeeLatestLog.out = dateNow;
-
-                            await employeeLatestLog.save();
-
-                            employee.latestLog = {
-                                reference: employeeLatestLog._id,
-                                date: employeeLatestLog.out
-                            }
-
-                            io.sockets.emit('employeeLog', {
-                                reference: employeeLatestLog._id,
-                                employee: employee.firstName,
-                                status: 'out'
-                            });
-
-                            return resolve({
-                                status: 200,
-                                message: `${employee.firstName} ${employee.lastName} checked-out at ${dateNow.toLocaleDateString()}`
-                            })
-
-                        } else {
-                            io.sockets.emit('logError', {
-                                message: "You cannot log multiple attendance in a single day!"
-                            });
-
-                            return reject({
-                                status: 400,
-                                message: `Multiple log violation.`
-                            })
+                        employee.latestLog = {
+                            reference: employeeLog._id,
+                            date: dateNow
                         }
+
+                        await employee.save();
+
+                        io.sockets.emit('employeeLog', {
+                            reference: employeeLog._id,
+                            employee: employee.firstName,
+                            status: 'in'
+                        });
+
+                        return resolve({
+                            status: 200,
+                            message: `Employee did not check-out yesterday at ${dateNow.toLocaleDateString()}. \n${employee.firstName} ${employee.lastName} checked in.`
+                        });
+
+                    } else if (!timeOut) {
+                        employeeLatestLog.out = dateNow;
+
+                        await employeeLatestLog.save();
+
+                        employee.latestLog = {
+                            reference: employeeLatestLog._id,
+                            date: employeeLatestLog.out
+                        }
+
+                        io.sockets.emit('employeeLog', {
+                            reference: employeeLatestLog._id,
+                            employee: employee.firstName,
+                            status: 'out'
+                        });
+
+                        return resolve({
+                            status: 200,
+                            message: `${employee.firstName} ${employee.lastName} checked-out at ${dateNow.toLocaleDateString()}`
+                        })
+
+                    } else {
+                        io.sockets.emit('logError', {
+                            message: "You cannot log multiple attendance in a single day!"
+                        });
+
+                        return reject({
+                            status: 400,
+                            message: `Multiple log violation.`
+                        })
                     }
                 }
             }
