f04754f932a9e8b1261f799fe27fe08557ceb6bc
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-03-17 23:47:15 +0800

    refactored createRefreshToken function to async syntax and added the "token renewal" for expired tokens.

--- utility/jwt.js
+++ utility/jwt.js
@@ -1,44 +1,53 @@
-const jwt   = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 // import model
 const { Token } = require("../db/models/Token");
 
-const createToken = (user,duration) => {
-    return jwt.sign(user, process.env.JWT_KEY,
-        {
-            expiresIn : duration
-        });
+
+const createToken = (user, duration) => {
+	let token = jwt.sign(user, process.env.JWT_KEY, {
+		expiresIn: duration
+	});
+
+	return token;
 }
 
-const createRefreshToken = (user) => {
-    const refToken =  jwt.sign(user, process.env.REFRESH_KEY);
-    
-    Token.findOneAndDelete({ email: user.email})
-    .then(token => {
-        if(token){
-            console.log('Deleted Refresh token')
-            const newRefToken = new Token({
-                email       : user.email,
-                token       : refToken
-            })
-            newRefToken.save()
-                .then(() => console.log('Succesfully saved refresh token'))
-                .catch(err => console.error(err))
-        } else {
-        if(!token){
-            const newRefToken = new Token({
-                email       : user.email,
-                token       : refToken
-            })
-            newRefToken.save()
-                .then(() => console.log('Succesfully saved refresh token'))
-                .catch(err => console.error(err))
-        }
-        }
-    })
+const createRefreshToken = async (email) => {
+	try {
+		// find matching refresh token
+		let refreshToken = await Token.findOne({ email });
+
+		// delete refresh token if expired...
+		if (refreshToken) {
+			jwt.verify(refreshToken, process.env.REFRESH_KEY, (err, decoded) => {
+				if (err.name === "TokenExpiredError") {
+					Token.findByIdAndDelete(refreshToken._id);
+				}
+			});
+		}
+
+		// sign a new refresh token
+		let newRefreshToken = jwt.sign(email, process.env.REFRESH_KEY, {
+			expiresIn: process.env.REFRESH_TOKEN_DURATION
+		});
+
+		// create a new refresh token
+		const db_refreshToken = new Token({
+			email: email,
+			token: newRefreshToken
+		});
+
+		// save new refresh token
+		db_refreshToken.save();
+		console.log('[SUCCESS] saved new refresh token to database.');
+
+	} catch (error) {
+		console.error(error);
+		console.log('[ERROR] unable to generate refresh token.')
+	}
 }
 
-module.exports = { 
-    createToken,
-    createRefreshToken
-  }
+module.exports = {
+	createToken,
+	createRefreshToken
+}
