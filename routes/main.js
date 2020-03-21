a69de3b914142ac7e929903283110966da078976
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-03-21 17:23:39 +0800

    updated routes for serving utility files

--- routes/main.js
+++ routes/main.js
@@ -1,34 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+
+// set path of the utility pages
+const basePath = path.join(__dirname, '../utility/pages');
 
 module.exports = (io) => {
 
    router.get('/', (req, res) => {
-      res.redirect('/log_employee.html');
+      res.sendFile(basePath + '/log_employee.html');
    });
-
+   
    router.get('/enroll-admin', (req, res) => {
-      res.redirect('/enroll_user.html');
+      console.log(basePath);
+      res.sendFile(basePath + '/enroll_user.html');
    });
 
    router.get('/enroll-employee', (req, res) => {
-      res.redirect('/enroll_employee.html');
+      res.sendFile(basePath + '/enroll_employee.html');
    });
 
-   router.get('/login-test', (req, res) => {
-      res.redirect('/test_login.html');
+   router.get('/login', (req, res) => {
+      res.sendFile(basePath + '/test_login.html');
    })
 
    router.get('/reset-password', (req, res) => {
-      res.redirect('/resetPass.html');
+      res.sendFile(basePath + '/resetPass.html');
    })
 
    router.get('/reset-password-key', (req, res) => {
-      res.redirect('/resetPassEmail.html');
+      res.sendFile(basePath + '/resetPassEmail.html');
    })
 
-   router.get('/import-csv', (req, res) => {
-      res.redirect('/mass_upload.html');
+   router.get('/csv', (req, res) => {
+      res.sendFile(basePath + '/mass_upload.html');
    })
 
 
