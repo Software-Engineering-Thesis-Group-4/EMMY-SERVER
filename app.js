91df6f4f96f10fca5a0877176d0ce3f820fba545
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-03-21 17:15:53 +0800

    added bootraper function as a wrapper to execute everything in order. first create connection to database before listening to incoming requests.

--- app.js
+++ app.js
@@ -9,14 +9,19 @@ const dotenv      = require('dotenv');
 const helmet      = require('helmet');
 const fileUpload  = require('express-fileupload');
 
-const app 	 = express();
+const app = express();
 const server = http.createServer(app);
-const io 	 = socketIO(server);
+const io = socketIO(server);
+const PORT = process.env.PORT || '3000';
 
-// LOAD ENVIRONMENT VARIABLES
+const { createDBConnection } = require('./db');
+const DB_NAME = process.env.DB_NAME || "Emmy";
+
+
+// LOAD ENVIRONMENT VARIABLES ---------------------------------------------------------------------------------
 dotenv.config();
 
-// APPLICATION LEVEL CONFIGURATIONS ---------------------------------------------------------------
+// APPLICATION CONFIGURATIONS ---------------------------------------------------------------------------------
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
@@ -26,62 +31,57 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(helmet());
 app.use(fileUpload({
-	debug		: true
+	debug: true
 }));
 
-// DATABASE ---------------------------------------------------------------------------------------
-const { createDBConnection } = require('./db');
-let DB_NAME = process.env.DB_NAME || "Emmy";
-createDBConnection(DB_NAME, process.env.DB_PORT);
-
-
-// DB backup runs every 2:00am ----- Timezone: Asia/Kuala Lumpur
-//require('./utility/cronScheduler');
-
-// IMPORT ROUTES ----------------------------------------------------------------------------------
+// IMPORT & CONFIGURE ROUTES ----------------------------------------------------------------------------------
 const employeeLogsRoute = require('./routes/employee-logs')(io);
-const employeeRoute     = require('./routes/employee')(io);
-const utilityRoute      = require('./routes/main')(io);
-const authRoute         = require('./routes/auth')(io);
-
+const employeeRoute = require('./routes/employee')(io);
+const utilityRoute = require('./routes/main')(io);
+const authRoute = require('./routes/auth')(io);
 
 app.use('/auth', authRoute);									// localhost:3000/auth/
-app.use('/utility', utilityRoute); 							// localhost:3000/utility
+app.use('/main', utilityRoute); 							// localhost:3000/utility
 app.use('/api/employees', employeeRoute); 				// localhost:3000/api/employees/
 app.use('/api/employeelogs', employeeLogsRoute); 		// localhost:3000/api/employeelogs/
 
 // TODO: add route for servering a single page application (Vue)
 // code here...
 
-/* CATCH 404 AND FORWARD REQUEST TO ERROR HANDLER --------------------------------------------------
-	DESCRIPTION:
-	- If the user tries to access unknown routes aside from the available routes above,
-	  it will be directed to this middleware.
-*/
+// CATCH 404 AND FORWARD REQUEST TO ERROR HANDLER -------------------------------------------------------------
 app.use((req, res, next) => {
 	next(createError(404));
 });
 
-
-/* ERROR HANDLER -----------------------------------------------------------------------------------
-	DESCRIPTION:
-	- This is the error handling middleware.
-*/
+// ERROR HANDLER ----------------------------------------------------------------------------------------------
 app.use((err, req, res, next) => {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-	// render the error page
+	
 	res.status(err.status || 500);
 	res.render('error');
 });
 
+// BOOSTRAPPER ------------------------------------------------------------------------------------------------
+async function bootstrap() {
+	try {
+		console.log("-------------------------------------------------------------------");
+		await createDBConnection(DB_NAME, process.env.DB_PORT);
 
-// LISTEN -----------------------------------------------------------------------------------------
-const PORT = process.env.PORT || '3000';
+		server.listen(PORT, () => {
+			console.log("-------------------------------------------------------------------");
+			console.log(`server listening on port: ${PORT}`);
+			console.log(`local: http://localhost:${PORT}/`);
+			console.log("-------------------------------------------------------------------");
+		});
 
-server.listen(PORT, () => {
-	console.log(`server listening on port: ${PORT}`);
-	console.log(`local: http://localhost:${PORT}/`);
-});
+	} catch (error) {
+		console.log('500 INTERNAL SERVER ERROR.');
+		console.error(error);
+		throw new Error(error);
+	}
+}
+
+// EXECUTE APPLICATION ---------------------------------------------------------------------------------------
+bootstrap();
