341f37a5a5b05e3174717809367c60c37b311928
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-04-22 00:21:34 +0800

    Merge branch 'testing' of https://github.com/Software-Engineering-Thesis-Group-4/EMMY-SERVER into kel

--- app.js
+++ app.js
@@ -1,40 +1,72 @@
-// const createError = require('http-errors');
-const http       = require("http");
-const path       = require("path");
-const logger     = require("morgan");
-const socketIO   = require("socket.io");
-const cors       = require("cors");
-const express    = require("express");
-const ip         = require("ip");
-const dotenv     = require("dotenv");
-const helmet     = require("helmet");
-const fileUpload = require("express-fileupload");
-const colors     = require("colors");
+const http       = require('http');
+const path       = require('path');
+const logger     = require('morgan');
+const socketIO   = require('socket.io');
+const cors       = require('cors');
+const express    = require('express');
+const ip         = require('ip');
+const helmet     = require('helmet');
+const fileUpload = require('express-fileupload');
+const colors     = require('colors');
+const RateLimit  = require('express-rate-limit');
+const MongoStore = require('rate-limit-mongo');
+
+colors.enable();
+const { createDBConnection } = require('./db');
 
 // LOAD ENVIRONMENT VARIABLES ---------------------------------------------------------------------------------
-const cfg = dotenv.config().parsed;
+const cfg = require('./configs/config.js');
 
 const app    = express();
 const server = http.createServer(app);
 const io     = socketIO(server);
 const PORT   = cfg.PORT || 3000;
-
-const mode 	= cfg.MODE;
-colors.enable();
-
-const { createDBConnection } = require("./db");
+const mode = cfg.MODE;
 
 // APPLICATION CONFIGURATIONS ---------------------------------------------------------------------------------
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(logger("dev"));
+app.use(RateLimit({
+	store: new MongoStore({
+		uri: `mongodb://localhost:${cfg.DB_PORT}/${cfg.DB_NAME}`,
+		collectionName: "expressRateLimitRecord"
+	}),
+	max: 100, //number of request threshold
+	windowMs: 15 * 60 * 1000, //15mins per 100request threshold
+	delayMs: 0
+}));
+
+app.use(helmet({
+	referrerPolicy: {
+		policy: 'same-origin'
+	},
+	contentSecurityPolicy: {
+		directives: {
+			defaultSrc: ["'self'"],
+			styleSrc: ["'self", "cdn.jsdelivr.net", "fonts.googleapis.com"],
+			scriptSrc: ["'self'", "cdn.jsdelivr.net"]
+		}
+	},
+	featurePolicy: {
+		features: {
+			fullscreen: ["'*'"],
+			vibrate: ["'none'"],
+			payment: ["'none'"],
+			camera: ["'none"],
+			geolocation: ["'none'"],
+			microphone: ["'none'"]
+		}
+	}
+}));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "client"))); // the directory for Vue
 app.use(cors());
 app.use(helmet());
-app.use(fileUpload((mode === 'dev' ? 
+app.use(fileUpload((mode === 'dev' ?
 	{ debug: true } : { debug: false })
 ));
 
@@ -57,16 +89,9 @@ app.use("/auth", authRoute); 								// localhost:3000/auth/
 app.use("/main", utilityRoute); 							// localhost:3000/utility
 app.use("/api/employees", employeeRoute); 			// localhost:3000/api/employees/
 app.use("/api/employeelogs", employeeLogsRoute); 	// localhost:3000/api/employeelogs/
-app.get(/.*/, (req, res) => {
-	// localhost:3000/* (for serving vue spa)
+app.get(/.*/, (req, res) => { 							// localhost:3000/* ----> (for serving vue spa)
 	res.sendFile(__dirname + "/client/index.html");
 });
-
-// CATCH 404 AND FORWARD REQUEST TO ERROR HANDLER -------------------------------------------------------------
-// REMOVE: Not sure if removing middleware will have serious side effects. will disable temporarily
-// app.use((req, res, next) => {
-// 	next(createError(404));
-// });
 
 // ERROR HANDLER ----------------------------------------------------------------------------------------------
 // app.use((err, req, res) => {
@@ -79,35 +104,44 @@ app.get(/.*/, (req, res) => {
 // });
 
 // BOOSTRAPPER ------------------------------------------------------------------------------------------------
-async function bootstrap() {
+async function start() {
 	try {
 		console.clear();
 		console.log("Starting Application...".black.bgGreen + "\nInitializing connection to database...");
 
 		// initialize database connection
-		let connection = await createDBConnection(cfg.DB_NAME, cfg.DB_PORT);
+		let { connection } = await createDBConnection(cfg.DB_NAME, cfg.DB_PORT);
 
 		console.log(
-			" SERVER RUNNING ".black.bgGreen + "\n" + 
-			"MongoDB Database: " + connection.connection.name + " (connected)".green
+			" SERVER RUNNING ".black.bgGreen + "\n" +
+			"\nMongoDB Database: " + connection.name.brightCyan
 		);
 
+		const environment = (process.env.NODE_ENV === 'development ') ? 'Development'.black.bgYellow : 'Production'.brightCyan;
 		const host_url = 'http://localhost:'.cyan + PORT.brightCyan;
-		const net_url = `http://:${ip.address()}:`.cyan + PORT.brightCyan;
+		const net_url = `http://${ip.address()}:`.cyan + PORT.brightCyan;
 
 		server.listen(PORT, () => {
 			console.log(
 				"--------------------------------------------------\n" +
-				"- local:   " + host_url + "\n" +
-				"- network: " + net_url + "\n" +
-				"--------------------------------------------------"
+				"- Environment: " + environment + "\n" +
+				"- local: " + host_url + "\n" +
+				"- network: " + net_url +
+				"\n--------------------------------------------------"
 			);
 		});
 	} catch (error) {
-		console.log("INTERNAL SERVER ERROR (500)".bgRed);
+		console.log(
+			"INTERNAL SERVER ERROR (500)".bgRed + "\n" +
+			"Please restart the server..." + "\n"
+		);
 		console.error(error);
 		throw new Error(error);
 	}
 }
 
-bootstrap();
+
+
+
+
+start();
--- utility/dbBackup.js
+++ utility/dbBackup.js
@@ -1,19 +1,19 @@
-const fs 		= require('fs');
-const _ 		= require('lodash');
+const fs        = require('fs');
+const _         = require('lodash');
 const childProc = require('child_process');
-const path 		= require('path');
-const zipFold	= require('zip-a-folder');
+const path      = require('path');
+const zipFold   = require('zip-a-folder');
 
 
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 
 
- 
+
 const dbOptions = {
 	user: false,                           // must have value for security, default to false for dev phase
 	pass: false,                           // must have value for security, default to false for dev phase
-	host: 'localhost',                    
+	host: 'localhost',
 	port: process.env.DB_PORT,
 	database: process.env.DB_NAME
 };
@@ -23,14 +23,14 @@ exports.zipBackup = async () => {
 
 	try {
 
-		const zipPath 	= path.join(__dirname + '/../downloadables/backup.zip');
-		const dbPath 	= path.join(__dirname, '/../db/backup/Emmy'); 
+		const zipPath = path.join(__dirname + '/../downloadables/backup.zip');
+		const dbPath = path.join(__dirname, '/../db/backup/Emmy');
 
 		await zipFold.zip(dbPath, zipPath)
 
 		console.log('Succesfully zipped backup folder');
 		return true;
-	
+
 	} catch (err) {
 		console.log(err);
 		return false;
@@ -40,15 +40,15 @@ exports.zipBackup = async () => {
 
 // backup database
 exports.dbAutoBackUp = async () => {
-	
+
 	try {
 
 		const dbPath = path.join(__dirname, '/../db/backup');
-		
+
 		// Command for mongodb dump process
 		let cmd = `mongodump --host ${dbOptions.host} --port ${dbOptions.port}  --db ${dbOptions.database} --out ${dbPath}`
-					
-		childProc.exec(cmd,{
+
+		childProc.exec(cmd, {
 			cwd: 'C:\\Program Files\\MongoDB\\Server\\4.2\\bin'
 		})
 
@@ -64,14 +64,14 @@ exports.dbAutoBackUp = async () => {
 
 // Restore database
 exports.dbRestore = async () => {
-	
+
 	try {
 		const uploadPath = path.join(__dirname, '/../uploads');
 
 		// Command for mongodb dump process
 		let cmd = `mongorestore --host ${dbOptions.host} --port ${dbOptions.port} --db ${dbOptions.database} ${uploadPath}`
 
-		childProc.exec(cmd,{
+		childProc.exec(cmd, {
 			cwd: 'C:\\Program Files\\MongoDB\\Server\\4.2\\bin'
 		})
 
@@ -87,7 +87,7 @@ exports.cleanUploads = () => {
 
 	const uploadPath = path.join(__dirname, '/../uploads')
 	console.log('hi')
-	if(fs.existsSync(uploadPath)){
+	if (fs.existsSync(uploadPath)) {
 		childProc.execSync('del /s /q ' + uploadPath);
 		console.log('Succesfully cleaned uploads folder');
 	}
