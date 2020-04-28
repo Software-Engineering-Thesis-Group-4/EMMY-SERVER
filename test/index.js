97052a7acb233f5c0a9d71d5abe9c81dc983d86d
Paolo Latoja <26647797+paololatoja@users.noreply.github.com>
2020-04-28 23:56:40 +0800

    fixed test/index.js

--- test/index.js
+++ test/index.js
@@ -2,20 +2,21 @@ const dotenv = require('dotenv');
 const { createDBConnection, closeDBConnection } = require("../db/index.js");
 const { insertRandomEmployees } = require("./test-generate_employees");
 const { insertEmployeeLogs } = require("./test-generate_logs");
+const path = require('path');
 
-let cfg = dotenv.config().parsed;
-
+let cfg = dotenv.config({ path: path.resolve(__dirname, '../configs/EMMY_DEV.env') }).parsed;
+console.log(cfg);
 
 async function start(entries) {
 
 	try {
 		// Initailize DB Connection
-		await createDBConnection(cfg.TEST_DB, cfg.DB_PORT);
-		
+		await createDBConnection(cfg.DB_NAME, cfg.DB_PORT);
+
 		// Perform Actions
 		await insertRandomEmployees(entries);
 		await insertEmployeeLogs();
-		
+
 		// Close DB Connection
 		console.log(`--------------------------------------------------------------------`)
 		await closeDBConnection();
