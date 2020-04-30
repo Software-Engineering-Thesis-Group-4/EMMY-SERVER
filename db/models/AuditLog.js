8f1725c2712de03ace2d0c590d023a4fd449bf26
mokiong <mokiong1427@gmail.com>
2020-05-01 02:24:10 +0800

    implementing audit-logs

--- db/models/AuditLog.js
+++ db/models/AuditLog.js
@@ -3,9 +3,11 @@ const Schema = mongoose.Schema;
 
 // AUDITLOG SCHEMA -----------------------
 const AuditLogSchema = Schema({
-   date     : { type : Date, default : Date.now },
-   message  : { type : String, required : true },
-   user     : { type: Schema.Types.ObjectId, ref: 'User' } 
+   date           : { type : Date, default : Date.now },
+   action         : { type : String, required : true },
+   description    : { type : String, required : true },
+   user           : { type : Schema.Types.ObjectId, ref: 'User' },
+   isServer       : { type : Boolean, default : false }
 });
 
 const AuditLog = mongoose.model('AuditLog', AuditLogSchema);
