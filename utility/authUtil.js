ca6c1dcfb568d53984c609e211b43f9b08ef635c
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-05-24 00:31:20 +0800

    removed console log

--- utility/authUtil.js
+++ utility/authUtil.js
@@ -2,78 +2,77 @@ const jwt = require('./jwt');
 const db = require('./mongooseQue');
 
 exports.verifyAdmin = async (req, res, next) => {
-    
-    const { userId,access_token } = req.body;
 
-    const verifiedToken = await jwt.verify(access_token,'authtoken'); 
+	const { userId, access_token } = req.body;
 
-    if(verifiedToken.value){
-        return res.status(401).send("Unauthorized Access.")
-    }
+	const verifiedToken = await jwt.verify(access_token, 'authtoken');
 
-    const user = await db.findById('user',{ _id : userId});
+	if (verifiedToken.value) {
+		return res.status(401).send("Unauthorized Access.")
+	}
 
-    if(user.value){
-        return res.status(401).send("Unauthorized Access.")
-    }
+	const user = await db.findById('user', { _id: userId });
 
-    return user.output.isAdmin ? next() : res.status(401).send("Unauthorized Access.") 
+	if (user.value) {
+		return res.status(401).send("Unauthorized Access.")
+	}
+
+	return user.output.isAdmin ? next() : res.status(401).send("Unauthorized Access.")
 }
 
 exports.verifyUser = async (req, res, next) => {
-    
-    const { userId,access_token } = req.body;
 
-    const verifiedToken = await jwt.verify(access_token,'authtoken'); 
+	const { userId, access_token } = req.body;
 
-    if(verifiedToken.value){
-        return res.status(401).send("Unauthorized Access.")
-    }
+	const verifiedToken = await jwt.verify(access_token, 'authtoken');
 
-    const user = await db.findById('user',{ _id : userId});
+	if (verifiedToken.value) {
+		return res.status(401).send("Unauthorized Access.")
+	}
 
-    if(user.value){
-        return res.status(401).send("Unauthorized Access.")
-    }
+	const user = await db.findById('user', { _id: userId });
 
-    return next(); 
+	if (user.value) {
+		return res.status(401).send("Unauthorized Access.")
+	}
+
+	return next();
 }
 
 exports.verifyUserGetMethod = async (req, res, next) => {
 
-    const { userId,access_token } = req.query;
-    console.log(userId + ' token ' + access_token)
+	const { userId, access_token } = req.query;
 
-    const verifiedToken = await jwt.verify(access_token,'authtoken'); 
+	const verifiedToken = await jwt.verify(access_token, 'authtoken');
 
-    if(verifiedToken.value){
-        return res.status(401).send("Unauthorized Access.")
-    }
+	if (verifiedToken.value) {
+		return res.status(401).send("Unauthorized Access.")
+	}
 
-    const user = await db.findById('user',{ _id : userId});
+	const user = await db.findById('user', { _id: userId });
 
-    if(user.value){
-        return res.status(401).send("Unauthorized Access.")
-    }
+	if (user.value) {
+		return res.status(401).send("Unauthorized Access.")
+	}
 
-    return next(); 
+	return next();
 }
 
 exports.verifyAdminGetMethod = async (req, res, next) => {
-    
-    const { userId,access_token } = req.query;
 
-    const verifiedToken = await jwt.verify(access_token,'authtoken'); 
+	const { userId, access_token } = req.query;
 
-    if(verifiedToken.value){
-        return res.status(401).send("Unauthorized Access.")
-    }
+	const verifiedToken = await jwt.verify(access_token, 'authtoken');
 
-    const user = await db.findById('user',{ _id : userId});
+	if (verifiedToken.value) {
+		return res.status(401).send("Unauthorized Access.")
+	}
 
-    if(user.value){
-        return res.status(401).send("Unauthorized Access.")
-    }
+	const user = await db.findById('user', { _id: userId });
 
-    return user.output.isAdmin ? next() : res.status(401).send("Unauthorized Access.") 
+	if (user.value) {
+		return res.status(401).send("Unauthorized Access.")
+	}
+
+	return user.output.isAdmin ? next() : res.status(401).send("Unauthorized Access.")
 }
