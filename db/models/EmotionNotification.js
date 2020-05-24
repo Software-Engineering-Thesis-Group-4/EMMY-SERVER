0021a1bf4dd8e6014b726103bcb5958189e2692c
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-05-25 04:00:18 +0800

    apply mongoose-autopopulate on emotion notification

--- db/models/EmotionNotification.js
+++ db/models/EmotionNotification.js
@@ -2,21 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const EmotionNotificationSchema = Schema({
-   dateCreated: Date,
-   employee: {
-    type: Schema.Types.ObjectId,
-    ref: 'Employee',
-    },
-    emotion: {
-      type: Number,
-      default: 0 //unsubmitted value
-    },
-    seenBy: []
- });
+	dateCreated: Date,
+	employee: {
+		type: Schema.Types.ObjectId,
+		ref: 'Employee',
+		autopopulate: true,
+	},
+	emotion: {
+		type: Number,
+		default: 0 //unsubmitted value
+	},
+	seenBy: []
+});
 
- const EmotionNotification = mongoose.model('EmotionNotificationLog', EmotionNotificationSchema);
+EmotionNotificationSchema.plugin(require('mongoose-autopopulate'));
 
- module.exports = {
-  EmotionNotificationSchema,
-  EmotionNotification
- }
+const EmotionNotification = mongoose.model('EmotionNotificationLog', EmotionNotificationSchema);
+
+module.exports = {
+	EmotionNotificationSchema,
+	EmotionNotification
+}
