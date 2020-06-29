77675b01dce953d6887d6ede90864faa5df62857
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-06-29 19:26:49 +0800

    automatically create a ROOT ADMIN user if there are no administators .

--- db/index.js
+++ db/index.js
@@ -1,16 +1,33 @@
 const mongoose = require('mongoose');
+const { User } = require('../db/models/User');
+const bcrypt = require('bcryptjs');
 require('colors');
 
 exports.createDBConnection = async (db_name, port) => {
 	try {
 		// scheduled server task
-		// require('../utility/cronScheduler');
 		const connection = await mongoose.connect(`mongodb://localhost:${port}/${db_name}`, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useCreateIndex: true,
 			useFindAndModify: false
 		});
+
+		// check if there are any existing administrators
+		const existingUsers = await User.findOne({ isAdmin: true });
+		const hashed_password = bcrypt.hashSync("administrator");
+		if (!existingUsers) {
+			const root_admin = new User({
+				firstname: "ROOT",
+				lastname: "ADMIN",
+				username: "ROOT_ADMIN",
+				email: "root_admin@emmy.com",
+				password: hashed_password,
+				isAdmin: true,
+			});
+
+			await root_admin.save();
+		}
 
 		console.clear();
 		return connection;
